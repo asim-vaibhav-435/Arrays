4b277fbc6a8c7ddc5253195219aae156e9e582e3
Asimvaibhav Tiwari <asimvaibhav435@gmail.com>
2021-12-09 23:23:09 +0530

    final

---
+++ arraytostring.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class arraytostring {
+    public static void main(String[] args) {
+        int arr2[] = {5, 10, 15, 20, 25, 30}; //O(1)
+        // initialize array
+
+        System.out.println("Arary as string: " + Arrays.toString(arr2)); //O(n)
+        // using array method to print as string
+    }
+}
