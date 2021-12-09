671afe2e3ff47d20964b03122cba133d9547bf08
Asimvaibhav Tiwari <asimvaibhav435@gmail.com>
2021-12-09 23:21:37 +0530

    Delete arraytostring.java

--- arraytostring.java
+++
@@ -1,9 +0,0 @@
-import java.util.Arrays;
-
-public class arraytostring {
-    public static void main(String[] args) {
-        int arr2[] = {5, 10, 15, 20, 25, 30}; //O(1)
-
-        System.out.println("Arary as string: " + Arrays.toString(arr2)); //O(n)
-    }
-}
